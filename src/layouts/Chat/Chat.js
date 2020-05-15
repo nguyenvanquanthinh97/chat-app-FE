12011185a1948b8a3ac6e9974b2a84abe31985f2
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-05-15 17:49:26 +0700

    fixing be routing

--- src/layouts/Chat/Chat.js
+++ src/layouts/Chat/Chat.js
@@ -25,7 +25,7 @@ const Chat = (props) => {
       history.replace('/auth/login');
     }
     if (token) {
-      socket = io(`${process.env.REACT_APP_BACKEND_CHAT_WHISPER || 'https://chat-whisper.herokuapp.com'}/${companyId}`, {
+      socket = io(`${process.env.REACT_APP_BACKEND_CHAT_WHISPER || 'https://chat-app-be.tk'}/${companyId}`, {
         query: {
           token: token
         }
