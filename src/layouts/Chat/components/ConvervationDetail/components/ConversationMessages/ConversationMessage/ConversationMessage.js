70ecf531f56cc7b44166ccdf62459adfab6e6166
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-07-21 21:38:13 +0700

    change receiver link color

--- src/layouts/Chat/components/ConvervationDetail/components/ConversationMessages/ConversationMessage/ConversationMessage.js
+++ src/layouts/Chat/components/ConvervationDetail/components/ConversationMessages/ConversationMessage/ConversationMessage.js
@@ -8,134 +8,121 @@ import { get } from 'lodash';
 import { Typography, Link, Avatar, colors } from '@material-ui/core';
 import { connect } from 'react-redux';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    marginBottom: theme.spacing(2)
-  },
-  authUser: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    '& $body': {
-      backgroundColor: theme.palette.primary.main,
-      color: theme.palette.primary.contrastText
-    }
-  },
-  inner: {
-    display: 'flex',
-    maxWidth: 500
-  },
-  avatar: {
-    marginRight: theme.spacing(2)
-  },
-  body: {
-    backgroundColor: colors.grey[100],
-    color: theme.palette.text.primary,
-    borderRadius: theme.shape.borderRadius,
-    padding: theme.spacing(1, 2)
-  },
-  content: {
-    marginTop: theme.spacing(1)
-  },
-  image: {
-    marginTop: theme.spacing(2),
-    height: 'auto',
-    width: 380,
-    maxWidth: '100%'
-  },
-  footer: {
-    marginTop: theme.spacing(1),
-    display: 'flex',
-    justifyContent: 'flex-end'
-  },
-  link: {
-    color: "#3A08A0"
-  }
+const useStyles = makeStyles((theme) => ({
+	root: {
+		marginBottom: theme.spacing(2)
+	},
+	authUser: {
+		display: 'flex',
+		justifyContent: 'flex-end',
+		'& $body': {
+			backgroundColor: theme.palette.primary.main,
+			color: theme.palette.primary.contrastText
+		}
+	},
+	inner: {
+		display: 'flex',
+		maxWidth: 500
+	},
+	avatar: {
+		marginRight: theme.spacing(2)
+	},
+	body: {
+		backgroundColor: colors.grey[100],
+		color: theme.palette.text.primary,
+		borderRadius: theme.shape.borderRadius,
+		padding: theme.spacing(1, 2)
+	},
+	content: {
+		marginTop: theme.spacing(1)
+	},
+	image: {
+		marginTop: theme.spacing(2),
+		height: 'auto',
+		width: 380,
+		maxWidth: '100%'
+	},
+	footer: {
+		marginTop: theme.spacing(1),
+		display: 'flex',
+		justifyContent: 'flex-end'
+	},
+	link: {
+		color: '#fff'
+	},
+	receiverLink: {
+		color: '#3A08A0'
+	}
 }));
 
 const ConversationMessage = ({ message, className, authAvatar, otherUserAvatar, ...props }) => {
+	const classes = useStyles();
+	const isAuth = get(message, 'sender.authUser', false);
 
-  const classes = useStyles();
-  const isAuth = get(message, 'sender.authUser', false);
-
-  return (
-    <div
-      className={clsx(
-        classes.root,
-        {
-          [classes.authUser]: isAuth
-        },
-        className
-      )}
-    >
-      <div className={classes.inner}>
-        <Avatar
-          className={classes.avatar}
-          component={RouterLink}
-          src={isAuth ? authAvatar : otherUserAvatar}
-          to="/profile/1/timeline"
-        />
-        <div>
-          <div className={classes.body}>
-            <div>
-              <Link
-                color="inherit"
-                component={RouterLink}
-                to="/profile/1/timeline"
-                variant="h6"
-              >
-                {get(message, 'sender.authUser') ? 'Me' : get(message, 'sender.username')}
-              </Link>
-            </div>
-            <div className={classes.content}>
-              {message.sender.contentType === 'image' && (
-                <img
-                  alt="Attachment"
-                  className={classes.image}
-                  src={message.sender.content}
-                />
-              )}
-              {message.sender.contentType === 'text' && (
-                <Typography
-                  color="inherit"
-                  variant="body1"
-                >
-                  {message.sender.content}
-                </Typography>
-              )}
-              {
-                message.sender.contentType === 'file' && (
-                  <Typography
-                    color="inherit"
-                    variant="body1"
-                  >
-                    <a className={classes.link} href={message.sender.content}>{message.sender.content}</a>
-                  </Typography>
-                )
-              }
-            </div>
-          </div>
-          <div className={classes.footer}>
-            <Typography
-              className={classes.time}
-              variant="body2"
-            >
-              {moment(message.sender.createdAt).fromNow()}
-            </Typography>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={clsx(
+				classes.root,
+				{
+					[classes.authUser]: isAuth
+				},
+				className
+			)}
+		>
+			<div className={classes.inner}>
+				<Avatar
+					className={classes.avatar}
+					component={RouterLink}
+					src={isAuth ? authAvatar : otherUserAvatar}
+					to="/profile/1/timeline"
+				/>
+				<div>
+					<div className={classes.body}>
+						<div>
+							<Link color="inherit" component={RouterLink} to="/profile/1/timeline" variant="h6">
+								{get(message, 'sender.authUser') ? 'Me' : get(message, 'sender.username')}
+							</Link>
+						</div>
+						<div className={classes.content}>
+							{message.sender.contentType === 'image' && (
+								<img alt="Attachment" className={classes.image} src={message.sender.content} />
+							)}
+							{message.sender.contentType === 'text' && (
+								<Typography color="inherit" variant="body1">
+									{message.sender.content}
+								</Typography>
+							)}
+							{message.sender.contentType === 'file' && (
+								<Typography color="inherit" variant="body1">
+									<a
+										className={isAuth ? classes.link : classes.receiverLink}
+										href={message.sender.content}
+									>
+										{message.sender.content}
+									</a>
+								</Typography>
+							)}
+						</div>
+					</div>
+					<div className={classes.footer}>
+						<Typography className={classes.time} variant="body2">
+							{moment(message.sender.createdAt).fromNow()}
+						</Typography>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 ConversationMessage.propTypes = {
-  className: PropTypes.string,
-  message: PropTypes.object.isRequired,
-  authImg: PropTypes.string
+	className: PropTypes.string,
+	message: PropTypes.object.isRequired,
+	authImg: PropTypes.string
 };
 
-const mapStateToProps = state => ({
-  authAvatar: state.auth.avatar
+const mapStateToProps = (state) => ({
+	authAvatar: state.auth.avatar
 });
 
 export default connect(mapStateToProps)(ConversationMessage);
